720e885a5d0d8874b9f5e8ba63109811808640f6
Samuel Beck <samcu00@yahoo.com>
2023-10-24 14:21:56 -0400

    get rid of the table!

--- src/Pages/Dashboard/Components/CountEditor.jsx
+++ src/Pages/Dashboard/Components/CountEditor.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./CountEditor.css";
 
 const CountEditor = ({ isOpen, onClose, onConfirm, onArchive, itemToEdit }) => {
   const [name, setName] = useState("My Name");
@@ -43,97 +44,70 @@ const CountEditor = ({ isOpen, onClose, onConfirm, onArchive, itemToEdit }) => {
         onSubmit={(e) => e.preventDefault()}
         className="text-tet-light flex flex-col items-center gap-6 rounded-lg bg-white p-6"
       >
-        <table cellPadding="5px" cellSpacing="5px">
-          <tbody>
-            <tr>
-              <td align="right" width="50%">
-                <label>Name: </label>
-              </td>
-              <td align="left" className="border">
-                <input
-                  type="text"
-                  placeholder="New Count"
-                  value={name}
-                  className="px-2 py-1 placeholder:text-text-dark/75"
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td align="right">
-                <label>Units:</label>
-              </td>
-              <td align="left" className="border">
-                <input
-                  type="text"
-                  placeholder="units"
-                  value={units}
-                  className="px-2 py-1"
-                  onChange={(e) => setUnits(e.target.value)}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td align="right">
-                <label>Function:</label>
-              </td>
-              <td align="left">
-                <select
-                  value={summaryFunction}
-                  onChange={(e) => setSummaryFunction(e.target.value)}
-                >
-                  <option value="sum">sum</option>
-                  <option value="average">average</option>
-                  <option value="min">min</option>
-                  <option value="max">max</option>
-                  <option value="median">median</option>
-                  <option value="count">count</option>
-                </select>
-              </td>
-            </tr>
-            <tr>
-              <td align="right">
-                <label>Period:</label>
-              </td>
-              <td align="left">
-                <select
-                  value={summaryPeriod}
-                  onChange={(e) => setSummaryPeriod(e.target.value)}
-                >
-                  <option value="daily">daily</option>
-                  <option value="weekly">weekly</option>
-                  <option value="monthly">monthly</option>
-                </select>
-              </td>
-            </tr>
-            <tr>
-              <td align="right">
-                <button
-                  className="rounded-lg bg-brand px-4 py-2 text-white"
-                  onClick={handleConfirm}
-                >
-                  {itemToEdit ? "Save" : "Create"}
-                </button>
-              </td>
-              <td align="left">
-                <button
-                  className="rounded-lg bg-brand px-4 py-2 text-white"
-                  onClick={onClose}
-                >
-                  Cancel
-                </button>
-                {itemToEdit && (
-                  <button
-                    className="rounded-lg bg-brand px-4 py-2 text-white"
-                    onClick={handleArchive}
-                  >
-                    Archive
-                  </button>
-                )}
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="grid">
+          <label className="left">Name: </label>
+          <input
+            type="text"
+            placeholder="New Count"
+            value={name}
+            className="right"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label className="left">Units:</label>
+          <input
+            type="text"
+            placeholder="units"
+            value={units}
+            className="right"
+            onChange={(e) => setUnits(e.target.value)}
+          />
+          <label className="left">Function:</label>
+          <select
+            className="right"
+            value={summaryFunction}
+            onChange={(e) => setSummaryFunction(e.target.value)}
+          >
+            <option value="sum">sum</option>
+            <option value="average">average</option>
+            <option value="min">min</option>
+            <option value="max">max</option>
+            <option value="median">median</option>
+            <option value="count">count</option>
+          </select>
+          <label className="left">Period:</label>
+          <select
+            value={summaryPeriod}
+            className="right"
+            onChange={(e) => setSummaryPeriod(e.target.value)}
+          >
+            <option value="daily">daily</option>
+            <option value="weekly">weekly</option>
+            <option value="monthly">monthly</option>
+          </select>
+          {!itemToEdit && (
+            <div className="fill">
+              <button className="b" onClick={handleConfirm}>
+                Create
+              </button>
+              <button className="b" onClick={onClose}>
+                Cancel
+              </button>
+            </div>
+          )}
+          {itemToEdit && (
+            <div className="fill">
+              <button className="b" onClick={handleConfirm}>
+                Save
+              </button>
+              <button className="b" onClick={onClose}>
+                Cancel
+              </button>
+              <button className="ba" onClick={handleArchive}>
+                Archive
+              </button>
+            </div>
+          )}
+        </div>
       </form>
     </dialog>
   );
